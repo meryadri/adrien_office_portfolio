6728a60498e0117d59a407f0381fc3223ac9cd93
NOBODIDI <adrienmeryusa@gmail.com>
2025-07-17 12:21:45 -0400

    contact form and footer completed

--- 3d-portfolio/src/App.jsx
+++ 3d-portfolio/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Contact />
+      <Footer />
     </main>
   );
 };
